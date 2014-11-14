ebc085bab94c3f1d8acbcba9842f88790968b556
Spencer <spencer.scorcelletti@salesforce.com>
2014-11-13 22:00:23 -0800

    added assume-yes to apt-get command

--- config/install_salt.sh
+++ config/install_salt.sh
@@ -5,7 +5,7 @@ sudo ln -s /bin/true /sbin/initctl
 sudo apt-get install python-software-properties
 sudo add-apt-repository ppa:saltstack/salt
 sudo apt-get update
-sudo apt-get install salt-master
-sudo apt-get install salt-minion
-sudo apt-get install salt-syndic
+sudo apt-get --assume-yes install salt-master
+sudo apt-get --assume-yes install salt-minion
+sudo apt-get --assume-yes install salt-syndic
 
